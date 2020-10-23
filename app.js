40959e23269dbee4acc5588a453fad748af6411c
Jonathan Proctor <jproctor212@gmail.com>
2020-10-22 21:21:02 -0700

    Adding react and ejs libraries

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
-const express = require('express');
-const chalk = require('chalk');
+import express from 'express';
+import chalk from 'chalk';
+import react from 'react';
+import reactDOM from 'react-dom';
+
 const app = express();
+
+const { green } = chalk;
+
+const { createElement } = react;
 
 const port = process.env.port || 3000;
 
+function Welcome(props) {
+    createElement(
+        'h1',
+        null,
+        props.name
+    )
+}
+
+function App() {
+    createElement(
+        'div',
+        null,
+        Welcome
+    )
+}
+
+if (typeof window === 'undefined') {
+    console.log('hello');
+    reactDOM.render(App, this.refs[this.app]);
+}
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send("Book Finder App!");
+    res.render(
+        "index",
+        {}
+    );
 });
 
 app.listen(port, () => {
-    console.log(`Running on port ${chalk.green(port)}`);
+    console.log(`Running on port ${green(port)}`);
 });
