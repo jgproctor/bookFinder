61e0cf25ec36263c690cb5967d3d688fe43ffd9b
Jonathan Proctor <jproctor212@gmail.com>
2020-10-24 22:03:44 -0700

    Creating router.

--- app.js
+++ app.js
@@ -2,15 +2,21 @@
 import express from 'express';
 import chalk from 'chalk';
 import morgan from 'morgan';
-import { } from './src/routes/bookRouter';
+import bodyParser from 'body-parser';
+import bookRouter from './src/routes/bookRouter.js';
 
 const app = express();
 const { green } = chalk;
 const port = process.env.port || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(morgan('tiny'));
+
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.render(
--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import searchBooks from '../services/goodreadsService.js';
 
 function router() {
   const bookRouter = express.router();
 
-  bookRouter.route('');
+  bookRouter.route('/search/books')
+    .get((req, res) => {
+      if (req.query.q) {
+        const results = searchBooks(req.query.q);
+        res.json(results);
+      } else {
+        const allBooks = searchBooks();
+        res.json(allBooks);
+      }
+    });
+
+  return bookRouter;
 }
 
 module.exports = router();
