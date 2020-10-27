42ef22dd3e61ef64304a87a63e6a7ef86d810bf6
Jonathan Proctor <jproctor212@gmail.com>
2020-10-26 21:28:30 -0700

    Set up search results view.

--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable import/extensions */
 const express = require('express');
 const searchBooks = require('../services/goodreadsService.js');
@@ -5,16 +6,24 @@ const searchBooks = require('../services/goodreadsService.js');
 function router() {
   const bookRouter = express.Router();
 
+  function renderResults(res, results) {
+    res.render(
+      'books.ejs',
+      {
+        results
+      }
+    );
+  }
+
   bookRouter.route('/search/books')
     .get((req, res) => {
       (async function search() {
         if (req.query.q) {
           const results = await searchBooks(req.query.q);
-          console.log(results);
-          res.json(results);
+          renderResults(res, results);
         } else {
-          const allBooks = await searchBooks();
-          res.json(allBooks);
+          const results = await searchBooks();
+          renderResults(res, results);
         }
       }());
     });
