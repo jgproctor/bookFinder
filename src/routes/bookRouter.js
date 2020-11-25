bbb9286bf56f0df20abcc447e01ac3ae0fd88902
Jonathan Proctor <jproctor212@gmail.com>
2020-11-25 15:17:43 -0700

    Using button for return home. Adding loading animation. Adding more info link.

--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-plusplus */
 /* eslint-disable import/extensions */
 import express from 'express';
@@ -25,7 +26,6 @@ export default function bookRouter() {
       (async function search() {
         if (req.query.q && req.query.q !== '') {
           const results = await searchBooks(req.query.q);
-          console.log(results);
           renderResults(res, results);
         } else {
           const results = await searchBooks();
